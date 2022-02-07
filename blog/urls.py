0289dc3bfa42c4b77d7bc0c3050026a736578f70
Mahendra Yadav <mahendrayadav7571@gmail.com>
2022-02-07 16:08:38 +0530

    make-templates-staticand app

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path("", views.bloghome, name='home'),
+    path("<str:slug>", views.blogpost, name='home')
+]
---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def bloghome(request):
+    return render(request, "blog/bloghome.html")
+def blogpost(request, slug):
+    return render(request, "blog/blogpost.html")
