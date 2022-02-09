2f36efb0210fb615f3f3df914053671fdde60e07
Mahendra Yadav <mahendrayadav7571@gmail.com>
2022-02-09 18:41:18 +0530

    added contactus page made models learn about message framework

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+from django.contrib import messages
 # Create your views here.
+from .models import Contact
+
+
 def home(request):
     return render(request, "home/home.html")
 
@@ -8,4 +11,20 @@ def about(request):
     return render(request, "home/about.html")
 
 def contact(request):
-    return render(request, "home/contact.html")
+
+        if request.method == "POST":
+            name = request.POST["name"]
+            phone = request.POST["phone"]
+            email = request.POST["email"]
+            desc = request.POST["textarea"]
+
+            if len(name)<3 or len(phone)<10 or len(desc)<10:
+                messages.error(request, 'Enter a valid input.')
+            else:
+                contact = Contact(name = name, email = email, phone = phone, textarea = desc)
+                contact.save()
+                messages.success(request, 'Your form has been submitted.')
+
+
+
+        return render(request, "home/contact.html")
